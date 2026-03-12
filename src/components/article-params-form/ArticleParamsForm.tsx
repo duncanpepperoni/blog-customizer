45efa4b3689b4eb67aed9674d7155c36565907b1
duncanpepperoni <IW83vPN3hi@tutamail.com>
2026-03-13 01:34:20 +0300

    completed project

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,135 @@
+import { OptionType } from 'src/constants/articleProps';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type FormState = {
+	fontFamilyOption: OptionType;
+	fontColor: OptionType;
+	backgroundColor: OptionType;
+	contentWidth: OptionType;
+	fontSizeOption: OptionType;
+};
+
+type ArticleParamsFormProps = {
+	isOpen: boolean;
+	formState: FormState;
+	fontFamilyOptions: OptionType[];
+	fontColors: OptionType[];
+	backgroundColors: OptionType[];
+	contentWidthArr: OptionType[];
+	fontSizeOptions: OptionType[];
+	onChange: (params: Partial<FormState>) => void;
+	onApply: () => void;
+	onReset: () => void;
+	onToggle: () => void;
+	onClose: () => void;
+};
+
+export const ArticleParamsForm = ({
+	isOpen,
+	formState,
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	onChange,
+	onApply,
+	onReset,
+	onToggle,
+}: ArticleParamsFormProps) => {
+	const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+		event.preventDefault();
+		onApply();
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+			<div className={styles.arrowButtonWrapper}>
+				<ArrowButton isOpen={isOpen} onClick={onToggle} />
+			</div>
+
+			<aside
+				className={`${styles.container} ${
+					isOpen ? styles.container_open : ''
+				}`}>
+				<div className={styles.header}>
+					<Text as='h2' size={25} weight={800} uppercase dynamicLite>
+						Задайте параметры
+					</Text>
+				</div>
+
+				<form className={styles.form} onSubmit={handleSubmit}>
+					{/* ШРИФТ */}
+					<div className={styles.field}>
+						<Select
+							title='Шрифт'
+							options={fontFamilyOptions}
+							selected={formState.fontFamilyOption}
+							onChange={(option) => onChange({ fontFamilyOption: option })}
+						/>
+					</div>
+
+					{/* РАЗМЕР ШРИФТА */}
+					<div className={styles.field}>
+						<RadioGroup
+							name='fontSize'
+							title='Размер шрифта'
+							options={fontSizeOptions}
+							selected={formState.fontSizeOption}
+							onChange={(option) => onChange({ fontSizeOption: option })}
+						/>
+					</div>
+
+					{/* ЦВЕТ ШРИФТА */}
+					<div className={styles.field}>
+						<Select
+							title='Цвет шрифта'
+							options={fontColors}
+							selected={formState.fontColor}
+							onChange={(option) => onChange({ fontColor: option })}
+						/>
+					</div>
+
+					{/* СЕПАРАТОР ПЕРЕД ЦВЕТОМ ФОНА */}
+					<div className={styles.separatorWrapper}>
+						<Separator />
+					</div>
+
+					{/* ЦВЕТ ФОНА */}
+					<div className={`${styles.field} ${styles.fieldColorBackground}`}>
+						<Select
+							title='Цвет фона'
+							options={backgroundColors}
+							selected={formState.backgroundColor}
+							onChange={(option) => onChange({ backgroundColor: option })}
+						/>
+					</div>
+
+					{/* ШИРИНА КОНТЕНТА */}
+					<div className={styles.field}>
+						<Select
+							title='Ширина контента'
+							options={contentWidthArr}
+							selected={formState.contentWidth}
+							onChange={(option) => onChange({ contentWidth: option })}
+						/>
+					</div>
+
+					<div className={styles.buttons}>
+						<Button
+							title='Сбросить'
+							type='clear'
+							htmlType='button'
+							onClick={onReset}
+						/>
+						<Button title='Применить' type='apply' htmlType='submit' />
 					</div>
 				</form>
 			</aside>
